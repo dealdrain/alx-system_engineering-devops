a5c440dcd2799ef04e43b819cc87e88b15d106dd
dealdrain <dealdrain@yahoo.com>
2024-01-19 08:07:01 -0800

    correction task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,14 @@
 # install flask 2.1.0 from pip3
 
+package { 'python':
+  ensure   => '3.8.10',
+  provider => 'pip3'
+}
 package { 'flask':
   ensure   =>  '2.1.0',
+  provider =>  'pip3',
+}
+package { 'Werkzeug':
+  ensure   =>  '2.1.1',
   provider =>  'pip3',
 }
