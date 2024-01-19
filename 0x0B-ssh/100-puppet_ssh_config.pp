12579413746c83962a383c89cde40731ea19e4a2
dealdrain <dealdrain@yahoo.com>
2024-01-19 06:23:58 -0800

    SSH tasks

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,9 @@
+# Puppet setting changes (Turns off password)
+file_line { 'disable password login':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+file_line { 'add path to find the keys':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
