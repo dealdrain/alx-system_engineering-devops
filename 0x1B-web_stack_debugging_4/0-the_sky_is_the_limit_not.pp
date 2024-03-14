fa97dd5c25f83ed87771b0b187f4a238d32cb5e6
dealdrain <dealdrain@yahoo.com>
2024-03-14 02:44:52 -0700

    correction task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 
 # UP the limit
 exec { 'file limit':
-  onlyif  => 'test -e /etc/default/nginx',
-  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+-> onlyif  => 'test -e /etc/default/nginx',
+-> command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
   provider => shell,
 }
